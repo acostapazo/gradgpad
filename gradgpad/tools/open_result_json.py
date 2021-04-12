047e8134e1bcb94d2a72c06e2cebd1acd1d9e0ff
Artur Costa-Pazo <acosta@alicebiometrics.com>
2021-04-12 08:52:38 +0200

    Add trace to open result json. Add pad-radar gif sample

--- gradgpad/tools/open_result_json.py
+++ gradgpad/tools/open_result_json.py
@@ -1,4 +1,5 @@
 import json
+import os
 import traceback
 
 from gradgpad.tools.fancy_protocol_correspondences import get_fancy_protocol
@@ -15,6 +16,9 @@ def open_result_json(filename, apply_fancy_names=True, verbose=False):
             print(f"Loaded result for {len(results.keys())} protocols from {filename}")
     except:  # noqa E722
         traceback.print_exc()
-        print(f"Not loaded anything from {filename}")
+        current_path = os.path.abspath(os.path.dirname(__file__))
+        print(f"Not loaded anything from {filename} (current_path: {current_path})")
+        gradgpad_path = os.getenv("GRADGPAD_PATH")
+        print(f"GRADGPAD_PATH: {gradgpad_path}")
         results = {}
     return results
